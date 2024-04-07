e5d209d80a0e8ac62ff123f0d528307baaeaea05
Rifazul <rifazul50@gmail.com>
2024-04-07 07:22:30 +0600

    our goals section half complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import OurGoals from "@/components/OurGoals/OurGoals";
 
 const Home = () => {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Navbar></Navbar>
+      <OurGoals></OurGoals>
     </div>
   );
 };
--- src/components/OurGoals/OurGoals.jsx
+++ src/components/OurGoals/OurGoals.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 const OurGoals = () => {
+  const [goals, setGoals] = useState();
+
+  useEffect(() => {
+    fetch("/goals.json")
+      .then((res) => res.json())
+      .then((data) => setGoals(data));
+  }, []);
+
+  //   console.log(goals);
   return (
-    <div>
-      <p> This Our Goals Section </p>
+    <div className="my-10 px-4 lg:px-40 ">
+      <div className="text-center">
+        <span className="text-red-600 border-red-400 border p-1 px-4 rounded-md cursor-pointer">
+          Goals
+        </span>
+        <h2 className="text-3xl font-semibold my-2 mt-3"> Our Goals</h2>
+      </div>
+      {/* card section */}
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+          {goals?.map((goal) => (
+            <Card key={goal.id} goal={goal}></Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
