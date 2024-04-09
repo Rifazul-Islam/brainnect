a9b2a6498584d164a748e268701c0717472563ac
ashraful islam <developerashrafulislamrifaz@gmail.com>
2024-04-09 20:54:01 +0600

    made goals section responsive and clean

--- src/components/OurGoals/OurGoals.js
+++ src/components/OurGoals/OurGoals.js
@@ -12,18 +12,17 @@ const OurGoals = () => {
       .then((data) => setGoals(data));
   }, []);
 
-  //   console.log(goals);
   return (
     <div className="my-10 px-4 lg:px-40 ">
       <div className="text-center">
-        <span className="text-red-600 border-red-400 border p-1 px-4 rounded-md cursor-pointer">
+        <span className="our_badge">
           Goals
         </span>
-        <h2 className="text-3xl font-semibold my-2 mt-3"> Our Goals</h2>
+        <h2 className="primary_title"> Our Goals</h2>
       </div>
       {/* card section */}
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+        <div className="service_card_container">
           {goals?.map((goal) => (
             <Card key={goal.id} goal={goal}></Card>
           ))}
