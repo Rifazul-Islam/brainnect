5a2a059a81fae519174a55275fa3db02ff2fcc9a
ashraful islam <developerashrafulislamrifaz@gmail.com>
2024-04-10 00:59:36 +0600

    added team sectiion in about us page

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,12 +1,14 @@
 import AboutUsBanner from "@/components/AboutUsBanner/AboutUsBanner";
 import OurGoals from "@/components/OurGoals/OurGoals";
+import TeamInfo from "@/components/TeamInfo/TeamInfo";
 
 const page = () => {
     return (
         <div>
             <AboutUsBanner></AboutUsBanner>
-            <div className="max-w-[90%] lg:max-w-[1300px] mx-auto">
+            <div className="max-w-[90%] lg:max-w-[1200px] mx-auto">
                 <OurGoals></OurGoals>
+                <TeamInfo></TeamInfo>
             </div>
         </div>
     );
