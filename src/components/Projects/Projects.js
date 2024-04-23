93750935c814f957a44f69b2f487baf2bfcce0e7
mursalinmirme <mursalinmir02@gmail.com>
2024-04-23 23:11:26 +0600

    project preview system implemented

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,26 @@
 "use client"
 import project1 from "@/assets/project1.jpg";
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect, useState } from "react";
+import { FaTabletAlt } from "react-icons/fa";
+import { FaLaptop, FaMobileScreenButton } from "react-icons/fa6";
 import styles from "./style.module.css";
-import Link from 'next/link'
 const Projects = () => {
+    const [previewDevice, setPreviewDevice] = useState("pc");
+    const [previewImage, setPreviewImage] = useState(null);
     const [projects, setProjects] = useState();
     useEffect(() => {
       fetch("/projects.json")
         .then((res) => res.json())
-        .then((data) => setProjects(data.projects));
+        .then((data) => {
+            setProjects(data?.projects);
+        });
     }, []);
     console.log(projects);
+    console.log(previewDevice);
     return (
+        <>
         <div className="w-[1300px] mx-auto">
           <div className="text-center">
               <span className="text-red-600 border-red-400 border p-1 px-4 rounded-md cursor-pointer">
@@ -25,7 +33,7 @@ const Projects = () => {
             {
                 projects?.map(project => (
                 <div key={project._id} id={styles.projectCard} className='bg-[#F0EEED] p-5 rounded-2xl h-[470px] relative overflow-hidden'>
-                    <Image id={styles.imageCov} width={"100%"} height={"100%"} src={project1} alt='project 1' />
+                    <Image id={styles.imageCov} width={300} height={100} src={'https://i.ibb.co/vj00X9s/unity-spark-22122-web-app-2.png'} alt='project 1' />
                     <hr />
                     <div id={styles.cover} className="w-full h-full bg-[#e2e2f79d] absolute -top-[500px] left-0 -z-0 transition duration-700 ease-in-out"></div>
                     <div className="transition z-10 absolute" id={styles.contentBox}>
@@ -33,7 +41,35 @@ const Projects = () => {
                         <h3 className='text-lg font-semibold'>{project?.name} - <span className='text-base'>A Human Resource Management Website</span></h3>
                     </div>
                     <div id={styles.buttons} className='flex gap-5 mt-5'>
-                        <button className="bg-[#FC4772] text-white px-4 py-2 rounded-lg">Preview</button>
+                        <button onClick={()=>document.getElementById('my_modal_2').showModal()} className="bg-[#FC4772] text-white px-4 py-2 rounded-lg">Preview</button>
+                        <dialog id="my_modal_2" className="modal">
+                            <div className={`modal-box ${previewDevice === 'pc' && 'max-w-[1000px]'} ${previewDevice === 'tab' && 'max-w-[780px]'} ${previewDevice === 'mob' && 'max-w-[425px]'}`}>
+                                <div className="flex gap-7 items-center justify-end">
+                                    <FaMobileScreenButton className={`text-xl ${previewDevice === 'mob' ? 'text-[#FC4772]' : 'text-black'}`} onClick={() => setPreviewDevice("mob")} />
+                                    <FaTabletAlt className={`text-xl ${previewDevice === 'tab' ? 'text-[#FC4772]' : 'text-black'}`}  onClick={() => setPreviewDevice("tab")} />
+                                    <FaLaptop className={`text-2xl ${previewDevice === 'pc' ? 'text-[#FC4772]' : 'text-black'}`} onClick={() => setPreviewDevice("pc")} />
+                                </div>
+                                <hr className="mt-5" />
+                                <Image
+                                    className="mt-5"
+                                    width={1000}
+                                    height={1000}
+                                    src={
+                                        previewDevice === 'pc'
+                                        ? project?.desktop_v_image
+                                        : previewDevice === 'tab'
+                                        ? project?.tablet_v_image
+                                        : previewDevice === 'mob'
+                                        ? project?.mobile_v_image
+                                        : '' // Add a default image source here if necessary
+                                    }
+                                    alt="preview image"
+                                    />
+                            </div>
+                            <form method="dialog" className="modal-backdrop">
+                                <button>close</button>
+                            </form>
+                            </dialog>
                         <Link href={project?.live_link}>
                         <button className="bg-[#FC4772] text-white px-4 py-2 rounded-lg">Live</button>
                         </Link>
@@ -45,6 +81,7 @@ const Projects = () => {
                 
            </div>
         </div>
+        </>
     );
 };
 
